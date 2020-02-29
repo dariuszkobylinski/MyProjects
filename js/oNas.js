21f6083f1880f64c7b602b73e6d253822e9a0d12
Dariusz Kobyliński <dariusz.kobylinski@wp.pl>
2020-02-29 23:36:31 +0100

    update

---
+++ js/oNas.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let activeMenuElement=document.querySelectorAll(".classLinkHref");
+    console.log(activeMenuElement);
+    activeMenuElement[0].style.color="darkorange";
+
+});
