27d336adb5fae72a7594f0edcc5f837cea18b6f2
Dariusz Kobyliński <dariusz.kobylinski@wp.pl>
2020-03-01 08:22:18 +0100

    update

---
+++ js/pricing.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let activeMenuElement=document.querySelectorAll(".classLinkHref");
+    // console.log(activeMenuElement);
+    activeMenuElement[2].style.color="darkorange";
+
+});
