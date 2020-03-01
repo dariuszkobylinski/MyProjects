f1fb05ab6e30bd40c580d237f6d8f493dff6444d
Dariusz Kobyliński <dariusz.kobylinski@wp.pl>
2020-03-01 18:16:34 +0100

    update

--- sendEmail.php
+++ sendEmail.php
@@ -1,16 +1,45 @@
 <?php
 
-  //Informację o emailu, na który będzie wysyłana wiadomość
-  $admin_email = "mdinvestments2018@gmail.com";
-  $email = $_REQUEST['email'];
-  $subject = $_REQUEST['subject'];
-  $comment = $_REQUEST['comment'];
+// Tworzymy zmienną dla imienia i nazwiska
+$name = $_POST['imienazwisko'];
 
-  //wysyłamy email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
+// Tworzymy zmienną dla adresu email
+$email = $_POST['adresEmail'];
 
-  //komunikat potwierdzający
-  echo "Dziękujemy za kontakt z nami!";
-  }
+// Tworzymy zmienną dla wiadomości
+$message = $_POST['message'];
 
+// Podajesz adres email z którego ma być wysłana wiadomość
+$odkogo = "dariusz.kobylinski@wp.pl";
+
+// Podajesz adres email na który chcesz otrzymać wiadomość
+$dokogo = "mdinvestments2018@gmail.com";
+
+// Podajesz tytuł jaki ma mieć ta wiadomość email
+$tytul = "Salon Kosmetyczny";
+
+// Przygotowujesz treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imie i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Dodajemy UTF-8 do naglowka naszej wiadomości
+
+$naglowek = "";
+$naglowek .= "Od:" . $odkogo . " \n";
+$naglowek .= "Content-Type:text/plain;charset=utf-8";
+
+// Wysyłamy wiadomość
+mail($dokogo, $tytul, $wiadomosc, $naglowek);
+//$sukces = mail($dokogo, $tytul, $wiadomosc, $naglowek);
+
+// Przekierowywujemy na potwierdzenie
+// if ($sukces){
+//   print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.html\">";
+// }
+// else{
+//   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+// }
 ?>
+
